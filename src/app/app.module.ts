701c4f5c00eb7476160a11da54063c306391e144
Ladoxer <ladderdoxer@gmail.com>
2024-06-22 11:03:36 +0530

    all layout set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ArticleCreateComponent } from './components/article-create/article-create.component';
+import { ArticleEditComponent } from './components/article-edit/article-edit.component';
+import { ArticleDetailComponent } from './components/article-detail/article-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    ArticleCreateComponent,
+    ArticleEditComponent,
+    ArticleDetailComponent
   ],
   imports: [
     BrowserModule,
